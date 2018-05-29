06f099109efacf28775775fab23f2df6d7b7465d
明高 王 <0sdf0@sina.cn>
2018-05-30 00:41:13 +0800

    demo-01

---
+++ UDPHelper/UDPHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPHelper
+{
+    public class UDPHelper
+    {
+        public IPAddress LocalAddress { get; set; }
+        public IPEndPoint LocalEndPoint { get; set; }
+        public UdpClient udpClient { get; set; }
+        public int port { get; set; }   //本地监听的Udp端口
+
+        //初始化，并绑定本地终结点
+        public UDPHelper()
+        {
+            //获取本地IP
+            IPAddress[] ips=Dns
+        }
+    }
+}
